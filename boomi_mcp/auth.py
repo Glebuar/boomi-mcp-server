d63f14dababe74c7b949e250effcdf79c8b0da21
Glebuar <glebuar@gmail.com>
2025-05-18 02:08:19 -0700

    update auth.py

--- boomi_mcp/auth.py
+++ boomi_mcp/auth.py
@@ -1,10 +1,11 @@
 import os
-from dotenv import load_dotenv, find_dotenv
+from dotenv import load_dotenv
 from boomi import Boomi
+
+load_dotenv()
 
 def get_client() -> Boomi:
     """Initialize and return a Boomi SDK client."""
-    load_dotenv(find_dotenv(usecwd=True))
     return Boomi(
         account=os.environ["BOOMI_ACCOUNT"],
         user=os.environ["BOOMI_USER"],
